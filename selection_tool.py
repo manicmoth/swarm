52aff21fb893ece1213656ceeec0c2dd1d8383f4
Alex Butler <lxbtlr@pm.me>
2023-02-09 17:16:39 -0500

    select rectangle gives coordinates

--- selection_tool.py
+++ selection_tool.py
@@ -5,6 +5,8 @@ import os
 # example code from https://mlhive.com/2022/04/draw-on-images-using-mouse-in-opencv-python
 ix,iy,sx,sy = -1,-1,-1,-1
 
+circles = True
+
 # mouse callback function
 def draw_lines(event, x, y, flags, param):
     global ix,iy,sx,sy
@@ -26,15 +28,55 @@ def draw_lines(event, x, y, flags, param):
             cv.line(img, (x, y), (sx, sy), (0, 0, 127), 2, cv.LINE_AA)
 
 
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing == True:
+            if mode == True:
+                pass
+                #cv.rectangle(img,(ix,iy),(x,y),(0,255,0),2)
+            else:
+                cv.circle(img,(x,y),5,(0,0,255),-1)
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        if mode == True:
+            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),1)
+            
+            print(f"RECTANGLE:\t\t({ix},{iy})\n \t\t\t({x},{y})")
+        else:
+            cv.circle(img,(x,y),5,(0,0,255),-1)
+
+
 path = os.getcwd()
 # read image from path and add callback
-img = cv.resize(cv.imread(path+"/test_images/image.jpg"), (1280, 720))
-cv.namedWindow('image') 
-cv.setMouseCallback('image',draw_lines)
+if not circles:
+    img = cv.resize(cv.imread(path+"/test_images/image.jpg"), (1280, 720))
+    cv.namedWindow('image') 
+    cv.setMouseCallback('image',draw_lines)
 
-while(1):
-    cv.imshow('image',img)
-    if cv.waitKey(20) & 0xFF == 27:
-        break
+    while(1):
+        cv.imshow('image',img)
+        if cv.waitKey(20) & 0xFF == 27:
+            break
 
-cv.destroyAllWindows()
+    cv.destroyAllWindows()
+
+else: 
+    img = np.zeros((512,512,3), np.uint8)
+    cv.namedWindow('image')
+    cv.setMouseCallback('image',draw_circle)
+    while(1):
+        cv.imshow('image',img)
+        k = cv.waitKey(1) & 0xFF
+        if k == ord('m'):
+            mode = not mode
+        elif k == 27:
+            break
+    cv.destroyAllWindows()
+
