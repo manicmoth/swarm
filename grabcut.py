10083bf864b3dea5333b4381c7b504012ced0b83
Alex Butler <lxbtlr@pm.me>
2023-02-09 16:28:42 -0500

    initial grabcut file

---
+++ grabcut.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import numpy as np
+
+# this program will select all objects of interest as marked by the user.
+# using black marks to indicate areas to be removed and white areas for areas to focus.
+# This tool is to be used with the selection tool
+
