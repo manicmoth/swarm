3e963ce4ab4202c6f8274ffc61c6f4cab1f3f5f6
kviiim <k82019@gmail.com>
2023-02-09 17:13:09 -0500

    grabcut function

--- grabcut.py
+++ grabcut.py
@@ -1,7 +1,29 @@
-import cv2 as cv
-import numpy as np
-
 # this program will select all objects of interest as marked by the user.
 # using black marks to indicate areas to be removed and white areas for areas to focus.
 # This tool is to be used with the selection tool
+import numpy as np
+import cv2 
+from matplotlib import pyplot as plt
 
+def grabcut(input_img, rect):
+    """ 
+    Takes an image and bounding box and returns a segmented image 
+
+    input_img - cv2 img 
+    rect - cv2 rect
+
+    returns - cv2 img
+    """
+    
+    mask = np.zeros(input_img.shape[:2],np.uint8)
+
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+
+    rect = (50,50,450,290)
+    cv2.grabCut(input_img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    output_img = input_img*mask2[:,:,np.newaxis]
+
+    return output_img
