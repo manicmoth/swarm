a8722f891608cd23e743f019cc49458b0a1fdb0d
kat <k82019@gmail.com>
2023-02-13 18:30:09 -0500

    clean up comments

--- detectron_segmenter.py
+++ detectron_segmenter.py
@@ -10,10 +10,8 @@ class Segmenter():
         self.cfg = get_cfg()
         self.cfg.MODEL.DEVICE = "cpu" ## mps or cuda depending on machine type
 
-        # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")) #"COCO-PanopticSegmentation/panoptic_fpn_R_50_1x.yaml")
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")) # alternative model "COCO-PanopticSegmentation/panoptic_fpn_R_50_1x.yaml")
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
         self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml") 
         self.predictor = DefaultPredictor(self.cfg)
 
