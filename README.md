a72c213d7f4e7a6303d4e5170c1d97107f4de279
Kat Mack <kat@Kats-MacBook-Pro.local>
2023-02-13 18:27:18 -0500

    refactor code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # segmentation_testing
+
+
+## Detectron Segmenter
+Requirements: 
+Detectron2 - https://detectron2.readthedocs.io/en/latest/tutorials/install.html
+- Python ≥ 3.7
+- PyTorch, torchvision ≥ 1.8
+OpenCV - Not needed for segmenter but needed for visualization
---
+++ detectron_segmenter.py
@@ -0,0 +1,36 @@
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+class Segmenter():
+    def __init__(self) -> None:
+        self.cfg = get_cfg()
+        self.cfg.MODEL.DEVICE = "cpu" ## mps or cuda depending on machine type
+
+        # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")) #"COCO-PanopticSegmentation/panoptic_fpn_R_50_1x.yaml")
+        self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml") 
+        self.predictor = DefaultPredictor(self.cfg)
+
+    def segment_image(self,im):
+        """
+        Finds instances of objects in given image and creates masks of each object 
+
+        im - cv image of size (3,n,m)
+        returns - a list of masks of size (n,m) of each detected object
+        """
+        outputs = self.predictor(im)
+
+        v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), scale=1.2)
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        masks = [mask.numpy() for mask in outputs["instances"].to("cpu").pred_masks]
+        return masks
+
+
+
+
