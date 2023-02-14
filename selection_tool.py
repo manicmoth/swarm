da6a964c7aa27c44dfc710dd23ebe7620bf0bece
Alex Butler <lxbtlr@pm.me>
2023-02-14 14:33:05 -0500

    updated comments

--- selection_tool.py
+++ selection_tool.py
@@ -2,53 +2,38 @@ import cv2 as cv
 import numpy as np
 import os
 import copy
+
 # Function takes in an image, allows selection of the subject and defining 
 # inclusions & exclusions
-
+# currently searches for image at img "image.jpg" and uses it as the canvas 
 # @param image input photo 
-# @return image marked up image  
-# @return coords
+# @return "savedImage.jpg" image marked up image  
+# @return rect_coords nested tuple of coords
 
-# TODO Currently, rect only stays on screen if its the last thing drawn (marker or rect),
-#       This should eventually apply last Rect coords onto the displayed image, unless a new rect is placed 
+# TODO parameterize to accept image thrown at it
+#      parameterize image size, to be standard (1080p)
+
+# TODO Refactor code into class
 
 color = True
 
 # example code from https://mlhive.com/2022/04/draw-on-images-using-mouse-in-opencv-python
-ix,iy,sx,sy = -1,-1,-1,-1
+ix,iy = -1,-1
 
-circles = True # toggle between line tool and bounding box, True = bounding box
-debug_mode = True # toggle helpful print statements
-drawing = False
-mode = False
+debug_mode = False  # toggle helpful print statements
+drawing = False     # System state variable 
+mode = False        # 
 file_name = "savedImage.jpg"
-path = os.getcwd() + "/test_images/"
+path = os.getcwd() + "/img/"
 file_name = path  +file_name
-
 colors = [( 0, 0, 0),
           ( 255, 255, 255)]
-    
-# mouse callback function
-def draw_lines(event, x, y, flags, param):
-    global ix,iy,sx,sy
-    global rect_coords
-    # if the left mouse button was clicked, record the starting
-    if event == cv.EVENT_LBUTTONDOWN:
+# read image from path and add callback
+img = cv.imread(path + "image.jpg")
+#img = np.zeros((512,512,3), np.uint8)
+cache = img
+rect_coords = ((-1,-1),(-1,-1))
 
-        # draw circle of 2px
-        cv.circle(img, (x, y), 3, (255, 255, 255), -1)
-     
-        if ix != -1: # if ix and iy are not first points, then draw a line
-            cv.line(img, (ix, iy), (x, y), (255, 255, 255), 2, cv.LINE_AA)
-        else: # if ix and iy are first points, store as starting points
-            sx, sy = x, y
-        ix,iy = x, y
-    elif event == cv.EVENT_LBUTTONDBLCLK:
-        ix, iy = -1, -1 # reset ix and iy
-        if flags == 33: # if alt key is pressed, create line between start and end points to create polygon
-            cv.line(img, (x, y), (sx, sy), (255,255,255), 2, cv.LINE_AA)
-
-# mouse callback function
 def draw_circle(event,x,y,flags,param):
     global ix,iy,drawing,mode
     global rect_coords 
@@ -77,7 +62,6 @@ def draw_circle(event,x,y,flags,param):
         if mode == True: 
             rect_coords = ((x,y),(ix,iy))
             
-            # TODO: make a copy of the image here and save that image
             if cache.any():
                 img = copy.deepcopy(cache)
             else:
@@ -88,44 +72,28 @@ def draw_circle(event,x,y,flags,param):
             
             if debug_mode:
                 print(f"RECTANGLE:\t({ix},{iy})\n \t\t({x},{y})")
-            else:
-                print(f"{ix}{iy}{x}{y}")
         else:
             cv.circle(img,(x,y),5,colors[color],-1)
     
 if debug_mode:
     print(file_name)
-# read image from path and add callback
-if not circles:
-    img = cv.imread(path+"/image.jpg")
-    cv.namedWindow('image') 
-    c = cv.setMouseCallback('image',draw_lines)
-    while(1):
-        cv.imshow('image',img)
-        if cv.waitKey(20) & 0xFF == 27:
-            break 
+cv.namedWindow('image')
+c = cv.setMouseCallback('image',draw_circle)
 
-else: 
-    img = np.zeros((512,512,3), np.uint8)
-    cache = img
-    rect_coords = ((-1,-1),(-1,-1))
-    cv.namedWindow('image')
-    c = cv.setMouseCallback('image',draw_circle)
-    
-    while(1):
-        cv.imshow('image',img)
-        k = cv.waitKey(1) & 0xFF
-        #print(f"{ix}{iy}")
-        if k == ord('m'):
-            mode = not mode
-        elif k == ord('c'):
-            color = not color
-        elif k == 27:
-            break
+while(1):
+    cv.imshow('image',img)
+    k = cv.waitKey(1) & 0xFF
+    #print(f"{ix}{iy}")
+    if k == ord('m'):
+        mode = not mode
+    elif k == ord('c'):
+        color = not color
+    elif k == 27:
+        break
   
 cv.imwrite( file_name, cache)
-print(rect_coords)
-          
+
 print("image saved")
 cv.destroyAllWindows()
+print(rect_coords)
 
