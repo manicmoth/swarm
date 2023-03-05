8cccb50a5df40c28dc1cffcda48912246d7b620d
kat <k82019@gmail.com>
2023-03-05 15:11:19 -0500

    start add layer functionality

--- ui.py
+++ ui.py
@@ -1,54 +1,67 @@
 import tkinter as tk
 from layer import Image_Layer
 
+class UserInterface():
+    def __init__(self) -> None:
+        self.window = tk.Tk(width=700, height=3500)
+        self.window.rowconfigure(0, weight=1, minsize=50)
+        for i in range(3):
+            self.window.columnconfigure(i, weight=1, minsize=75)
+
+        self.frame_output = tk.Frame(
+            master=self.window,
+            relief=tk.RAISED,
+            borderwidth=1,
+            background="blue"
+        )
+        self.frame_output.grid(row=0, column=0, columnspan = 2, padx=5,pady=5, sticky="nsew")
+        self.label_output = tk.Label(master=self.frame_output, text=f"Space for background image")
+        self.label_output.pack()
+
+        self.frame_label = tk.Frame(
+            master=self.window,
+            relief=tk.RAISED,
+            borderwidth=1,
+            background="red"
+        )
+        self.frame_label.grid(row=0, column=2, padx=5,pady=5, sticky="nsew")
+        self.label_layers = tk.Label(master=self.frame_label, text=f"Layer list")
+        self.label_layers.pack()
+        self.button_new_layer = tk.Button(master=self.frame_label, text="New Layer", command=self.new_layer_callback)
+        self.button_new_layer.pack()
 
 
-def new_layer_callback():
-    """
-    popup new layer definition
-    """
-    top=tk.Toplevel(window)
-    top.geometry("700x250")
-    top.title("New Layer")
-    #Create a label in Toplevel window
-    tk.Button(master=top, text="Select Layer Mask", command=open_mask_callback)
-
-def open_mask_callback():
-    """
-    Open mask image
-    """    
-    filename = tk.filedialog.askopenfilename(initialdir = "/",
-                                          title = "Select a File",
-                                          filetypes = (("Text files",
-                                                        "*.txt*"),
-                                                       ("all files",
-                                                        "*.*")))
-
-window = tk.Tk()
-window.rowconfigure(0, weight=1, minsize=50)
-for i in range(3):
-    window.columnconfigure(i, weight=1, minsize=75)
-
-frame_output = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1,
-    background="blue"
-)
-frame_output.grid(row=0, column=0, columnspan = 2, padx=5,pady=5, sticky="nsew")
-label_output = tk.Label(master=frame_output, text=f"Space for background image")
-label_output.pack()
+        self.layers = []
+        self.new_layer = Image_Layer()
 
 
-frame_label = tk.Frame(
-    master=window,
-    relief=tk.RAISED,
-    borderwidth=1,
-    background="red"
-)
-frame_label.grid(row=0, column=2, padx=5,pady=5, sticky="nsew")
-label_layers = tk.Label(master=frame_label, text=f"Layer list")
-label_layers.pack()
-button_new_layer = tk.Button(master=frame_label, text="New Layer", command=new_layer_callback)
+        self.window.mainloop()
 
-window.mainloop()
+
+    def new_layer_callback(self):
+        """
+        popup new layer definition
+        """
+        top=tk.Toplevel(self.window)
+        top.geometry("700x700")
+        top.title("New Layer")
+        #Create a label in Toplevel window
+        tk.Button(master=top, text="Select Layer Mask", command=self.open_mask_callback)
+
+    def open_mask_callback(self):
+        """
+        Open mask image
+        """    
+        filename = self.open_image("Select Layer Mask")
+
+    def open_image(self, title = "Choose an image"):
+        filename = tk.filedialog.askopenfilename(initialdir = "/",
+                                            title = title,
+                                            filetypes = (("Image files",
+                                                            "*.jpg *.jpeg *.png")))
+        return filename
+
+if __name__ == "__main__":
+    print("hihihi")
+    ui = UserInterface()
+    print(ui.layers)
