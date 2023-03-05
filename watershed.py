cf9fb1a51a0300f64e79e175c8993ca211ae327c
Alex Butler <lxbtlr@pm.me>
2023-03-05 16:57:08 -0500

    Add clean doc strings and imports

--- watershed.py
+++ watershed.py
@@ -1,6 +1,5 @@
 import cv2 as cv
-import numpy as np
-
+from numpy import ones, uint8
 # using distance transform with a watershed algorithm to produce segmented images
 # Given an image (edited or not) identify objects from background, then identify borders
 # this function will be used with the selection tool.
@@ -10,8 +9,10 @@ class WatershedSegmenter():
 
     def __init__(self, im)-> None:
         """
-        Initializes segmenter, performing initial calculations to produce 
-        a sure bg and fg from an image
+        Initializes watershed segmenter, performs initial calculations to produce 
+        a sure bg and fg from an image.
+        
+        im - cv image of size (n,m,3)
         """
         
         self.img = im
@@ -19,7 +20,7 @@ class WatershedSegmenter():
         self.ret, self.tresh = cv.threshold(self.gray, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
         
         # noise removal 
-        self.kernal = np.ones((3,3),np.uint8)
+        self.kernal = ones((3,3),uint8)
         # TODO: test effect of changing iterations
         self.opening =cv.morphologyEx(self.thresh, cv.MORPH_OPEN, self.kernal, iterations=2)
         
@@ -32,11 +33,16 @@ class WatershedSegmenter():
         self.dist_transform = cv.distanceTransform(self.opening,cv.DIST_L2,5)
         self.ret, self.sure_fg = cv.threshold(self.dist_transform,
                                               self.scalar*self.dist_transform.max(),255,0)
-        self.sure_fg = np.uint8(self.sure_fg)
+        self.sure_fg = uint8(self.sure_fg)
         self.unknown = cv.subtract(self.sure_bg,self.sure_fg)
        
 
     def segment(self)-> tuple:
+        """
+        Delinates objects in foreground from background, giving an image with 
+        color values mapping this deliniation, also gives an image of only the 
+        borders imposed ontop of foreground objects.
+        """
         self.ret, self.markers = cv.connectedComponents(self.sure_fg)
         self.markers +=1
         
