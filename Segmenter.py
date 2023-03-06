28f030bd08d118158396d8e2a3c9e6bfc7413a8c
Alex Butler <lxbtlr@pm.me>
2023-03-06 16:37:10 -0500

    Add grabcut comments

---
+++ Segmenter.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+
+# local imports
+import watershed as ws
+import detectron_segmenter as ds
+import grabcut as gcs
+
+
+
+#One segmenter class to wrap, one segmenter class to bind them...
+
+class Segmenter():
+
+    def __init__(self,):
+        
+        gcs.GrabCutSegmenter()
+
+        pass
+
+
+    def autoSegmenter(self):
+
+        pass
+
+    def manualSegmenter(self):
+
+
+        pass
+
+    def msegment_grabCut(self,):
+
+
+        pass
+
+    def msegment_watershed(self,):
+
+
+        pass
+
+
+
+
--- grabcut.py
+++ grabcut.py
@@ -5,19 +5,11 @@ from os import getcwd
 
 class GrabCutSegmenter():
 
-    def __init__(self, im, ) -> None:
+    def __init__(self,) -> None:
         """
         Initializes segmenter, and performs first pass of grabcut segmentation 
         using a bounding box
-        @param im - cv image of size (3,n,m)
-        @param rec_coords tuple of two xy-coordinate pairs that define the 
-        the bounding box
         """
-        if im == 0:
-            raise Exception("Please pass an img to the segmenter")
-        
-        # self.img is reserved for output image
-        self.og_image = im
         # define vars
         self.height, self.width = self.og_image.shape[:2]
         self.mask = np.zeros(self.height, self.width, np.uint8)
@@ -26,14 +18,22 @@ class GrabCutSegmenter():
         # perform first grabcut
         print("GrabCutSegmenter: all vars init")
 
-    def rect_segment(self, rec_coords:tuple = ()):
+    def rect_segment(self, im,rec_coords:tuple = ()):
         """
         Performs first pass of grabCut segmentation using a bounding box, gives
         insight to how grabCut will process image 
         
+        @param im - cv image of size (3,n,m)
+        @param rec_coords tuple of two xy-coordinate pairs that define the 
+        the bounding box
+        
         @return img - masked image size (n,m,3)
         """
         
+        if im == 0:
+            raise Exception("Please pass an img to the segmenter") 
+        # self.img is reserved for output image
+        self.og_image = im
         if rec_coords == ():
             raise Exception("Please define a bounding rectangle")
 
@@ -46,12 +46,17 @@ class GrabCutSegmenter():
         print("GrabCutSegmenter: rect_segment complete") 
         return self.img 
 
-    def mask_segment(self, newMask):
+    def mask_segment(self,im, newMask):
         '''
         Creates an updated mask of an object using grabCut segmentation and a 
         user defined mask. Assumes using the same image as rect_segment.
-        newMask - cv image of size (n,m,3)
+
+        @param im - cv image of size (3,n,m)
+        @param newMask - cv image of size (n,m,3)
         '''
+        if im == 0:
+            raise Exception("Please pass an img to the segmenter") 
+        self.og_image = im
         # toggle mask where there is black/white
         self.mask[newMask == 255] = 1
         self.mask[newMask == 0] = 0
